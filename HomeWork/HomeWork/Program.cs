9f3c09ec6d1e6d396dad691980bc844c6fa8812b
Nabiullin Damir <cdbacjdorz@yandex.ru>
2022-09-26 15:18:34 +0300

    Homework

---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Honework
+{
+    class Lesson
+    {
+        static void Main()
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            if ((y <= 1 && y >= 0 && Math.Abs(x) <= 1) && (Math.Abs(x) + Math.Abs(y) <= 1))
+            {
+                Console.WriteLine("True");
+            }
+            else if (Math.Abs(x) <= 1 && ((y >= -1 * x * x) && (y < 0)))
+            {
+                Console.WriteLine("True");
+
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
