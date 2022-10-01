332689e38e74b22014ca77c3d977dc005f49d0cf
Nabiullin Damir <cdbacjdorz@yandex.ru>
2022-10-01 11:15:00 +0300

    My Homework

---
+++ HomeworkToday/HomeworkToday/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Project
+{
+    class Lesson
+    {
+        static void CorrectPi(double epsil)
+        {
+            int k;
+            double sum = 0;
+            double resultFormula = 0;
+            double  befoResultFormula= 0;
+            for (k = 1; Math.Abs(resultFormula - befoResultFormula) > epsil; ++k)
+            {
+                befoResultFormula = resultFormula;
+                resultFormula = 1 / ((4 * k - 2) * (4 * k - 1));
+                sum += resultFormula;
+            }
+            sum = 8 * sum + 2 * Math.Log(2);
+            Console.WriteLine(sum);
+        }
+        static void Main()
+        {
+            CorrectPi(0.001);
+        }
+    }
+}
